0bc17839ce3509fe0bed3443e6cf63ee43bf9805
DvDs20 <kvietkauskasdeividas@gmail.com>
2021-02-15 09:17:23 +0200

    Finished design, started coding cipher

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,17 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
 public class Controller {
+    @FXML
+    private TextArea textField;
+
+    @FXML
+    private TextArea ciphertextField;
+
+    public void encryptButtonPressed(ActionEvent actionEvent) {
+
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,12 +6,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import static backEnd.cesearCipher.encrypt;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Casear Cipher");
         primaryStage.setScene(new Scene(root));
         primaryStage.setResizable(false);
         primaryStage.show();
@@ -20,5 +22,10 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
+        String text = "123";
+        int s = 4;
+        System.out.println("Text  : " + text);
+        System.out.println("Shift : " + s);
+        System.out.println("Cipher: " + encrypt(text, s));
     }
 }
