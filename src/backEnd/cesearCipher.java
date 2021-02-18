c148a5efab2ca53db476454cc65589b20b0dd14f
DvDs20 <kvietkauskasdeividas@gmail.com>
2021-02-19 01:23:29 +0200

    labas

--- src/backEnd/cesearCipher.java
+++ src/backEnd/cesearCipher.java
@@ -2,65 +2,31 @@ package backEnd;
 
 public class cesearCipher {
 
-    public static StringBuffer encrypt(String text, int s) {
-        StringBuffer result = new StringBuffer();
-        for (int i=0; i<text.length(); i++)
-        {
-            if (Character.isUpperCase(text.charAt(i)))
-            {
-                char ch = (char)(((int)text.charAt(i) +
-                        s - 65) % 26 + 65);
-                result.append(ch);
+    public static  String encrypt(String text, int shift) throws Exception {
+        String encrypted = "";
+        if (!String.valueOf(shift).isEmpty()){
+            if (shift > 0 && shift < 189) {
+                for (int i = 0; i < text.length(); i++) {
+                    int c = text.charAt(i) + shift;
+                    if (c > 126) {
+                        c -= 95;
+                    } else if (c < 32) {
+                        c += 95;
+                    }
+                    encrypted += (char) c;
+                }
             }
-            else if (Character.isWhitespace(text.charAt(i))) {
-                char ch = ' ';
-                result.append(ch);
-            }
-            else
-            {
-                char ch = (char)(((int)text.charAt(i) +
-                        s - 97) % 26 + 97);
-                result.append(ch);
+            else {
+                throw new Exception("Bad shift! Type another!");
             }
         }
-        return result;
+        else {
+            throw new Exception("Bad shift! Type another!");
+        }
+        return encrypted;
     }
 
-    public static StringBuffer decrypt(String text, int s) {
-        StringBuffer result = new StringBuffer();
-        for (int i=0; i<text.length(); i++)
-        {
-            if (Character.isUpperCase(text.charAt(i)))
-            {
-                char ch = (char)(((int)text.charAt(i) -
-                        s - 65) % 26 + 65);
-                result.append(ch);
-            }
-            else if (Character.isWhitespace(text.charAt(i))) {
-                char ch = ' ';
-                result.append(ch);
-            }
-            else
-            {
-                char ch = (char)(((int)text.charAt(i) -
-                        s - 97) % 26 + 97);
-                result.append(ch);
-            }
-        }
-        return result;
-    }
-
-    public static String encryptusingASCII(String text, int shift) {
-        String encryptedText = "";
-        for (int i = 0; i < text.length(); i++) {
-            int c = text.charAt(i) + shift;
-            if (c > 126) {
-                c -= 95;
-            } else if (c < 32) {
-                c += 95;
-            }
-            encryptedText += (char) c;
-        }
-        return encryptedText;
+    public static String decrypt (String encrypted, int shift) throws Exception {
+        return encrypt(encrypted, -shift);
     }
 }
--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -6,6 +6,11 @@ import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
+
+import javax.swing.*;
 
 public class Controller {
 
@@ -14,37 +19,48 @@ public class Controller {
     public TextArea textField;
     public TextField shiftTextField;
     public Button decryptButton;
-    public RadioButton withArrayButton;
+    public Button copyButton;
+    DropShadow shadow = new DropShadow();
 
-    public void encryptButtonPressed(ActionEvent actionEvent) {
+    public void encryptButtonPressed(ActionEvent actionEvent) throws Exception {
 
-        if (withArrayButton.isSelected()){
-            String text = textField.getText();
-            int shift = Integer.parseInt(shiftTextField.getText());
-            cesearCipher.encryptusingASCII(text, shift);
-            String ciphertext = String.valueOf(cesearCipher.encrypt(text, shift));
-            secondTextField.clear();
-            secondTextField.setText(ciphertext);
-        }
-        else {
+        try {
             String text = textField.getText();
             int shift = Integer.parseInt(shiftTextField.getText());
 
             String ciphertext = String.valueOf(cesearCipher.encrypt(text, shift));
-
             secondTextField.clear();
             secondTextField.setText(ciphertext);
+        }
+        catch (Exception exception) {
+            JOptionPane.showMessageDialog(null, exception.getMessage());
         }
 
     }
 
-    public void decryptButtonPressed(ActionEvent actionEvent) {
-        String ciphertext = textField.getText();
-        int shift = Integer.parseInt(shiftTextField.getText());
+    public void decryptButtonPressed(ActionEvent actionEvent) throws Exception {
+        try {
+            String ciphertext = textField.getText();
+            int shift = Integer.parseInt(shiftTextField.getText());
 
-        String text = String.valueOf(cesearCipher.decrypt(ciphertext, shift));
+            String text = String.valueOf(cesearCipher.decrypt(ciphertext, shift));
+            secondTextField.clear();
+            secondTextField.setText(text);
+        }
+        catch (Exception exception) {
+            JOptionPane.showMessageDialog(null, exception.getMessage());
+        }
 
-        secondTextField.clear();
-        secondTextField.setText(text);
     }
+
+    public void copyButtonPressed(ActionEvent actionEvent) {
+        String copyText = secondTextField.getText();
+
+        final Clipboard clipboard = Clipboard.getSystemClipboard();
+        final ClipboardContent content = new ClipboardContent();
+        content.putString(copyText);
+        clipboard.setContent(content);
+    }
+
+
 }
